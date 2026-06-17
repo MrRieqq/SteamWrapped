02efbc7c156372bc228a1c7efc2fb09514d9f9dc
Ермолин Лев Дмитриевич <ermolin-ld@college.tepk.dev>
2026-06-17 14:54:48 +0700

    First commit

---
+++ SteamWrapped/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SteamWrapped
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
