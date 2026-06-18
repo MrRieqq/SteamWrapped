4f9937fea660f30db18adf471d1f2b11996aa6c8
MrRieqq <leo.bankai337@gmail.com>
2026-06-18 16:33:52 +0300

    Доработка функционала кнопок

--- SteamWrapped/Pages/GamesPage.xaml.cs
+++ SteamWrapped/Pages/GamesPage.xaml.cs
@@ -25,4 +25,14 @@ public partial class GamesPage : ContentPage
             await _vm.LoadGames(AppData.SteamId);
         }
     }
+    private async void FilterButtonTapped(object sender, TappedEventArgs e)
+    {
+        if (sender is TapGestureRecognizer tap &&
+            tap.Parent is Border border)
+        {
+            await border.ScaleTo(0.92, 80, Easing.CubicOut);
+            await border.ScaleTo(1.0, 120, Easing.CubicIn);
+        }
+    }
+
 }
