b5d9c45607aeef3a28dd4e842f93ed8f2090ce9e
klimov-kv <klimov-kv@MOIT0204>
2026-06-18 09:00:14 +0700

    Добавление шрифтов

--- SteamWrapped/MauiProgram.cs
+++ SteamWrapped/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace SteamWrapped
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Orbitron-Bold.ttf", "Orbitionblod");
                 });
 
 #if DEBUG
