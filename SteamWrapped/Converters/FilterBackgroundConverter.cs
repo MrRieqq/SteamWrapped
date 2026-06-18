2a87f6d8d58fe452c0f139c417801eec5ee3635c
MrRieqq <leo.bankai337@gmail.com>
2026-06-18 16:33:18 +0300

    Создание анимаций

---
+++ SteamWrapped/Converters/FilterBackgroundConverter.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+namespace SteamWrapped.Converters;
+
+public class FilterBackgroundConverter : IValueConverter
+{
+    public object Convert(
+        object value,
+        Type targetType,
+        object parameter,
+        CultureInfo culture)
+    {
+        if (value?.ToString() == parameter?.ToString())
+            return Color.FromArgb("#355CFF");
+
+        return Color.FromArgb("#121B2B");
+    }
+
+    public object ConvertBack(
+        object value,
+        Type targetType,
+        object parameter,
+        CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+    public class ViewModeBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            bool isGridView = (bool)value;
+            string mode = parameter?.ToString();
+
+            bool isActive =
+                (mode == "Grid" && isGridView) ||
+                (mode == "List" && !isGridView);
+
+            return Color.FromArgb(
+                isActive ? "#355CFF" : "#121B2B");
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
---
+++ SteamWrapped/Converters/FilterTextConverter.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+namespace SteamWrapped.Converters;
+
+public class FilterTextConverter : IValueConverter
+{
+    public object Convert(
+        object value,
+        Type targetType,
+        object parameter,
+        CultureInfo culture)
+    {
+        if (value?.ToString() == parameter?.ToString())
+            return Colors.White;
+
+        return Color.FromArgb("#9FB0CC");
+    }
+
+    public object ConvertBack(
+        object value,
+        Type targetType,
+        object parameter,
+        CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+    public class ViewModeTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            bool isGridView = (bool)value;
+            string mode = parameter?.ToString();
+
+            bool isActive =
+                (mode == "Grid" && isGridView) ||
+                (mode == "List" && !isGridView);
+
+            return Color.FromArgb(
+                isActive ? "#FFFFFF" : "#8D9AB4");
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
