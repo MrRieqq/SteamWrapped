9e1882c81dcbd57084f2c547526c01a38b1acac2
MrRieqq <leo.bankai337@gmail.com>
2026-06-19 15:37:27 +0300

    Добавление метода

---
+++ SteamWrapped/ViewModels/AboutPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Globalization;
+using Microsoft.Maui.ApplicationModel;
+using System.Reflection;
+
+public partial class AboutViewModel : ObservableObject
+{
+    public string BuildDate =>
+        GetBuildDate().ToString("dd MMMM yyyy", new CultureInfo("ru-RU"));
+
+    private static DateTime GetBuildDate()
+    {
+        return File.GetLastWriteTime(
+            Assembly.GetExecutingAssembly().Location);
+    }
+}
