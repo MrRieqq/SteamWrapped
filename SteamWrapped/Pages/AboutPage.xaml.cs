9664126cb3648bd7f2fae70741aff0345f952193
klimov-kv <klimov-kv@MOIT0204>
2026-06-19 10:19:26 +0700

    Изменение страницы о приложении и добавление ссылок для связи

--- SteamWrapped/Pages/AboutPage.xaml.cs
+++ SteamWrapped/Pages/AboutPage.xaml.cs
@@ -2,8 +2,27 @@ namespace SteamWrapped.Pages;
 
 public partial class AboutPage : ContentPage
 {
-	public AboutPage()
-	{
-		InitializeComponent();
-	}
+    public Command OpenSteamApiCommand { get; }
+    public Command OpenMauiCommand { get; }
+    public Command OpenFaqCommand { get; }
+    public Command OpenGitCommand { get; }
+
+    public AboutPage()
+    {
+        InitializeComponent();
+
+        OpenSteamApiCommand = new Command(async () =>
+            await Launcher.OpenAsync("https://steamcommunity.com/dev"));
+
+        OpenMauiCommand = new Command(async () =>
+            await Launcher.OpenAsync("https://dotnet.microsoft.com/apps/maui"));
+
+        OpenFaqCommand = new Command(async () =>
+            await Launcher.OpenAsync("https://telegra.ph/SteamWrapped--FAQ-06-19"));
+
+        OpenGitCommand = new Command(async () =>
+            await Launcher.OpenAsync("https://github.com/MrRieqq/SteamWrapped"));
+
+        BindingContext = this;
+    }
 }
